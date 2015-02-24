52f6e94221ce7ae0623354fee4cd520ab55666af
Jakob Gaardsted <jgaardsted@gmail.com>
2015-02-25 00:16:58 +0100

    todo class

---
+++ src/todo.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "todo.h"
+
+/* this file is for todo notes. any stray thoughts that spring to mind.
+ - carrying too much weight should trigger encumbrance, related to strength,
+ and make e.g. weapon-attacks and defenses harder.
+- heavy weapons should require more strength to handle well.
+
+- obj type should switch from straight type to objdesc.(requires design.)
+
+- money should have a purpose, necessary/useful things to buy, at e.g. town level,
+so you get a cycle of hoarding heavy stuff, and selling it at town level,
+and useful things to buy at town level.
+consider alternate reality.
+
+persistence.. start by  saving few things, e.g. stats.
+I consider putting e.g. stats on labelled lines
+ - i can ignore the label, but use it for verification.
+
+
+*/
+
+todo::todo()
+{
+}
+
+
+todo::~todo()
+{
+}
---
+++ src/todo.h
@@ -0,0 +1,8 @@
+#pragma once
+class todo
+{
+public:
+  todo();
+  ~todo();
+};
+
