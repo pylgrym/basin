55c6588b315a8b87afaa39e23987612ee05a7c60
Jakob Gaardsted <jgaardsted@gmail.com>
2015-04-01 09:34:33 +0200

    pretty up keyboard layout

--- src/Cmds.cpp
+++ src/Cmds.cpp
@@ -965,55 +965,63 @@ bool HelpCmd::Do(std::ostream& err) {
   */
 
   std::string s[] = {
+    "",
     " ½ 1 2 3 4 5 6 7 8 9 0 +(?)   ",
     "Tab Q W E R T Y U I O P Å ¨   ",
     "     A S D F G H J K L Æ Ø '  ",
     "    < Z X C V B N M , . -     ",
+    "",
     "Q-stats, W-ear, E-xamine, T-akeoff, I-nv,",
     "O-pen/close,",
     "A-ctivate, S-ee, D-rop, F-bash, G-et,",
-    "Z-ap, X-spells, C-stairs."
+    "Z-ap, X-spells, C-stairs.",
+    "",
+    "@"
   };
 
-  for (int i = 0; i < 8; ++i) {
-    Cuss::prtL(s[i].c_str());
+  for (int i = 0; i < 100; ++i) {
+    std::string L = s[i];
+    if (L == "@") { break;  }
+    Cuss::prtL(L.c_str());
   }
 
   // Movement keys:
+  const int oy = 1;
   COLORREF moveHue = RGB(0, 185, 0);
-  Cuss::setCellBg(CPoint(15, 3), moveHue); // H
-  Cuss::setCellBg(CPoint(17, 3), moveHue); // J
-  Cuss::setCellBg(CPoint(19, 3), moveHue); // K
-  Cuss::setCellBg(CPoint(21, 3), moveHue); // L
+  Cuss::setCellBg(CPoint(15, 3 + oy), moveHue); // H
+  Cuss::setCellBg(CPoint(17, 3 + oy), moveHue); // J
+  Cuss::setCellBg(CPoint(19, 3 + oy), moveHue); // K
+  Cuss::setCellBg(CPoint(21, 3 + oy), moveHue); // L
 
-  Cuss::setCellBg(CPoint(14, 4), moveHue); // BN
-  Cuss::setCellBg(CPoint(16, 4), moveHue);
+  Cuss::setCellBg(CPoint(14, 4 + oy), moveHue); // BN
+  Cuss::setCellBg(CPoint(16, 4 + oy), moveHue);
 
-  Cuss::setCellBg(CPoint(14, 2), moveHue); // YU
-  Cuss::setCellBg(CPoint(16, 2), moveHue);
+  Cuss::setCellBg(CPoint(14, 2 + oy), moveHue); // YU
+  Cuss::setCellBg(CPoint(16, 2 + oy), moveHue);
 
   COLORREF oHue = RGB(185, 0, 185); // commands I could remove!
   COLORREF iHue = RGB(185, 0, 0);
+
   // O is Open:
-  Cuss::setCellBg(CPoint(20, 2), iHue); // O,open/close(!)
-  Cuss::setCellBg(CPoint(18, 2), oHue); // I,inventory // COULD be dropped! (A instead)
-  Cuss::setCellBg(CPoint(12, 2), iHue); // T is take off
-  Cuss::setCellBg(CPoint( 8, 2), iHue); // E examine
+  // (p is unequip - why/how?)
+  Cuss::setCellBg(CPoint(20, 2 + oy), iHue); // O,open/close(!)
+  Cuss::setCellBg(CPoint(18, 2 + oy), oHue); // I,inventory // COULD be dropped! (A instead)
+  Cuss::setCellBg(CPoint(12, 2 + oy), iHue); // T is take off
+  Cuss::setCellBg(CPoint(8, 2 + oy), iHue); // E examine
 
-  Cuss::setCellBg(CPoint(6, 2), oHue); // W = wear // COULD be dropped!(A instead)
-  Cuss::setCellBg(CPoint(4, 2), iHue); // Q = stats
+  Cuss::setCellBg(CPoint(6, 2 + oy), oHue); // W = wear // COULD be dropped!(A instead)
+  Cuss::setCellBg(CPoint(4, 2 + oy), iHue); // Q = stats
 
-  Cuss::setCellBg(CPoint(5, 3), iHue); // A = activate-use
-  Cuss::setCellBg(CPoint(7, 3), iHue); // S see/look
-  Cuss::setCellBg(CPoint(9, 3), iHue); // D drop
-  Cuss::setCellBg(CPoint(11, 3), iHue); // F bash
-  Cuss::setCellBg(CPoint(13, 3), iHue); // G get
+  Cuss::setCellBg(CPoint(5, 3 + oy), iHue); // A = activate-use
+  Cuss::setCellBg(CPoint(7, 3 + oy), iHue); // S see/look
+  Cuss::setCellBg(CPoint(9, 3 + oy), iHue); // D drop
+  Cuss::setCellBg(CPoint(11, 3 + oy), iHue); // F bash
+  Cuss::setCellBg(CPoint(13, 3 + oy), iHue); // G get
 
-  Cuss::setCellBg(CPoint(6, 4), iHue); // Z zap=cast spell
-  Cuss::setCellBg(CPoint(8, 4), oHue); // X spell-book(not nec?) // COULD be dropped! (z instead.)
-  Cuss::setCellBg(CPoint(10, 4), iHue); // C stairs, go up/down
+  Cuss::setCellBg(CPoint(6, 4 + oy), iHue); // Z zap=cast spell
+  Cuss::setCellBg(CPoint(8, 4 + oy), oHue); // X spell-book(not nec?) // COULD be dropped! (z instead.)
+  Cuss::setCellBg(CPoint(10, 4 + oy), iHue); // C stairs, go up/down
 
-  // p is unequip - why/how?
 
 	TheUI::promptForAnyKey(__FILE__, __LINE__, "help-pause");
 	Cuss::clear(true);
