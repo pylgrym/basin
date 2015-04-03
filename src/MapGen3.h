ca17f9f41948e4a8ec660e2a794eb9b7f80792c7
Jakob Gaardsted <jgaardsted@gmail.com>
2015-04-03 09:00:55 +0200

    mapgen3

---
+++ src/MapGen3.h
@@ -0,0 +1,8 @@
+#pragma once
+class MapGen3
+{
+public:
+  MapGen3();
+  ~MapGen3();
+};
+
--- src/cellmap/cellmap.cpp
+++ src/cellmap/cellmap.cpp
@@ -161,39 +161,6 @@ void Map::initOuterBorders() {
 
 
 
-void Map::initTunnels2(int level) {
-  GrCanvas laby; // FIXME - same problem with width/height!
-
-  runSimu(laby, NULL); // CDC* dc);
-  /* now do something with laby, transfer it to cellmap. */
-  // Create floor/environ.
-  for (int x = 0; x < Width; ++x) {
-    for (int y = 0; y < Height; ++y) {
-      CPoint cur(x, y);
-      Cell& cell = (*this)[cur]; 
-      if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1) { // The outer border.
-        cell.envir.type = EN_Border;
-      } else { // Inside-area:
-        CPoint p(x, y);
-
-        EnvirEnum etype = EN_Floor;
-        bool isWall = laby[p].isMarked();
-        switch (isWall) { // laby[p].c%3) {
-        case true: etype = EN_Wall;  break; // M_Unvisited
-        case false: etype = EN_Floor; break;
-        }
-        cell.envir.type = etype; // (isWall ? EN_Wall : EN_Floor);
-
-        if (!isWall && rnd::oneIn(9)) {
-          addObjAtPos(cur, level);
-        }
-
-      }
-    }
-  }
-  addStairs(Map::Width, Map::Height);
-}
-
 
 
 
